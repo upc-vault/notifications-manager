7834ab4fcaf5214c9aa2e9d9ba182e87c4b59dca
fixedbyte <wilmerquispecode@gmail.com>
2025-11-28 02:21:23 -0500

    feat(docker): add basic dockerfile

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 from flask import Flask, request
 from model.notification import Notification
 from pydantic import TypeAdapter
+import redis
 
 app = Flask(__name__)
 
 
-@app.route('/communications/v0/notifications', methods=['POST'])
-def sendNotification():
+@app.route('/notifications/v0/send-notifications', methods=['POST'])
+def sendNotifications():
     notifications_adapter = TypeAdapter(list[Notification])
     notifications = notifications_adapter.validate_json(request.body)
-    print(notifications)
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 
 if __name__ == '__main__':
